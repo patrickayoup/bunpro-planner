0a8dd0c216afbd24269eb56b1f9b6781e84a3343
Patrick Ayoup <patrick.ayoup@gmail.com>
2018-08-18 17:26:18 -0400

    Added bar chart

--- src/bunpro-planner.js
+++ src/bunpro-planner.js
@@ -6,19 +6,21 @@
 // @author Patrick Ayoup
 // @include *bunpro.jp/users*
 // @grant none
-// @require http://code.jquery.com/jquery-3.3.1.min.js
+// @run-at document-end
+// @require https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js
+// @require https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.10/lodash.min.js
 // ==/UserScript==
 (()=> {
+$.noConflict();
 
-    
 function toAbsoluteTime(relativeTime) {
     let absoluteTime = null;
     relativeTime = relativeTime.toLowerCase();
     if (relativeTime === 'now') {
         absoluteTime = moment();
     } else {
-        // Format: X hours
         let tokens = relativeTime.split(" ");
         let value = tokens[0];
         let unit = tokens[1];
@@ -28,26 +30,137 @@ function toAbsoluteTime(relativeTime) {
     return absoluteTime.format();
 }
 
-
 function getReviews() {
-    let cards = $('div .srs-info .row');
+    let cards = jQuery('div .srs-info .row');
     return cards.map(function () {
-        // Each card has 2 children, one for times correct, the other for next review time.
-        let kids = $(this).children();
+        let kids = jQuery(this).children();
         let rv = {
-            timesCorrect: $(kids[0]).text().trim().replace("Times Correct: ", ""),
+            timesCorrect: jQuery(kids[0]).text().trim().replace("Times Correct: ", ""),
             nextReview: toAbsoluteTime(
-                $(kids[1]).text().trim().replace("Next Review: ", "").replace("about ", "")
+                jQuery(kids[1]).text().trim().replace("Next Review: ", "").replace("about ", "")
             )
         };
         return rv;
     });
 }
 
+function insertChartNode() {
+    let showUpcomingGrammarButton = jQuery('div .show-upcoming-grammar').first();
+    let container = jQuery('<div id="bunpro-planner-chart-container"></div>');
+    let chart = jQuery('<canvas id="bunpro-planner-chart"></canvas>');
+    container.append(chart);
+    container.insertBefore(showUpcomingGrammarButton);
+    return chart;
+}
 
-$(document).ready(function () {
-    let reviews = getReviews();
-    console.log(reviews);
-});
+function generateChartLabels() {
+    let thisHour = moment().hour();
+    let hours = [];
+    for (i = thisHour; i < thisHour + 24; ++i) {
+        hours.push(i % 24);
+    }
+    return hours;
+}
 
+function getReviewsPerHour(reviews, labels) {
+    let now = moment();
+    let thisHour = now.hour();
+
+    // Filter for cards scheduled in the next 24 hours.
+    let filteredReviews = _.filter(reviews, function (card) {
+        return moment(card.nextReview).diff(now, 'hours') <= 24;
+    });
+    let groups = _.groupBy(filteredReviews, function (card) {
+        return moment(card.nextReview).hour();
+    });
+
+    return _.map(labels, function(hour) {
+        if (groups[hour]) {
+            return groups[hour].length;
+        } else {
+            return 0;
+        }
+    });
+}
+
+function buildChart(chart, reviews) {
+    var ctx = chart.get(0).getContext('2d');
+    let labels = generateChartLabels();
+    let myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Reviews',
+                data: getReviewsPerHour(reviews, labels),
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+}
+
+let reviews = getReviews();
+let chart = insertChartNode();
+buildChart(chart, reviews);
 })();
